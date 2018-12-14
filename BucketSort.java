29f2620844663227d9133e4e2467c01798e17164
BenBrode <45552051+BenBrode@users.noreply.github.com>
2018-12-14 15:00:16 +0800

    Update BucketSort.java

--- BucketSort.java
+++ BucketSort.java
@@ -1,1 +1,38 @@
+先便利數組，確定最大值
+確定長度，最大值+1
+創造新的數組，計數；每找到一次數，在對應的數組上+1
+輸出數組裡不為0的
+int temp = 0;
+for(i = 0; i < a.length; i++){
+  if(a.get(i)>temp){
+    temp = a.get(i);
+  }
+}
+int []b = new int[temp+1];
+for(n = 0; n < b.length; b++){
+  if(a[i]==b.indexOf(
+    b[n]++;
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
